69d50ba2fddd1a239af09f02b89ce6a3555de38b
Moondancer <moon@codedancer.de>
2025-02-19 19:03:12 +0100

    add chat retrieval, remove unused airium requirement

--- deepchat/chat.py
+++ deepchat/chat.py
@@ -6,19 +6,31 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from deepchat.db import get_db
+from markupsafe import Markup
 
+from .utility import template
 from . import ollama
+from .db import get_messages
 
 bp = Blueprint('chat', __name__)
 
 @bp.route('/')
 def index() -> str:
-    db = get_db()
-    
-    print(ollama.get_models())
+    return template(current_app.app_context(), 'base.html')
 
-    return render_template('base.html', title='DeepChat', models=ollama.get_models())
+@bp.route('/<string:chat_id>')
+def chat(chat_id: str) -> str:
+    messages = get_messages(chat_id)
+
+    msgs = Markup("")
+    for message, isUser in messages:
+        msgs += Markup(f"<div class='{'user-message' if isUser else 'bot-message'}'>")
+        msgs += Markup("<p>")
+        msgs += message
+        msgs += Markup("</p>")
+        msgs += Markup("</div>")
+
+    return template(current_app.app_context(), 'base.html', chat_box=msgs)
 
 @bp.route('/request', methods=('POST',))
 def do_prompt() -> Generator[str, None, None] | str:
@@ -28,6 +40,5 @@ def do_prompt() -> Generator[str, None, None] | str:
         prompt = data['prompt']
     except KeyError:
         return {"error": "Invalid request"}, 400
-    db = get_db()
     response = ollama.generate_response(model, prompt, current_app.app_context())
     return response
